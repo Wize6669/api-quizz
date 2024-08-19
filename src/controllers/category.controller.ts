a41a4101a142e6f7f20ee6e016bc448639bc4789
XERF99 <francisco.rodriguez@epn.edu.ec>
2024-08-19 09:17:03 -0500

    feat: Implements services, controllers, and routes for category functionality

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,76 @@
+import {Request, Response} from 'express';
+import {
+  createCategoryService,
+  categoryListService,
+  updateCategoryService,
+  getCategoryByIdService,
+  deleteCategoryService
+} from "../services/category.service";
+
+const createCategoryController = async (req: Request, res: Response) => {
+  const {name} = req.body;
+  const result = await createCategoryService({name})
+
+  if ('error' in result) {
+    return res.status(result.code).json({message: result.error});
+  }
+  res.status(201).json({message: 'Category created successfully.'});
+}
+
+const updateCategoryController = async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const {name} = req.body;
+  const numericId = parseInt(id, 10);
+
+  if (isNaN(numericId)) {
+    return res.status(400).json({error: 'Invalid ID format'});
+  }
+
+  const result = await updateCategoryService({id: numericId, name})
+  if ('error' in result) {
+    return res.status(result.code).json({message: result.error});
+  }
+  res.status(200).json(result);
+}
+
+const deleteCategoryController = async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const numericId = parseInt(id, 10);
+
+  if (isNaN(numericId)) {
+    return res.status(400).json({error: 'Invalid ID format'});
+  }
+  const result = await deleteCategoryService(numericId);
+  if ('error' in result) {
+    return res.status(result.code).json({message: result.error});
+  }
+  res.status(result.code);
+}
+
+const listCategoryController = async (req: Request, res: Response) => {
+  const {page, count} = req.query;
+  const pageAux = Number(page);
+  const countAux = Number(count);
+  const result = await categoryListService(pageAux, countAux);
+  if ('error' in result) {
+    return res.status(result.code).json({message: result.error});
+  }
+  res.status(200).json(result);
+}
+
+const getCategoryByIdController = async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const numericId = parseInt(id, 10);
+
+  if (isNaN(numericId)) {
+    return res.status(400).json({error: 'Invalid ID format'});
+  }
+  const result = await getCategoryByIdService(numericId);
+  if ('error' in result) {
+    return res.status(result.code).json({message: result.error});
+  }
+
+  res.status(200).json(result);
+}
+
+export { createCategoryController, updateCategoryController, deleteCategoryController, listCategoryController, getCategoryByIdController};
---
+++ src/routes/category.route.ts
@@ -0,0 +1,20 @@
+import {Router} from 'express';
+import {
+  createCategoryController,
+  listCategoryController,
+  deleteCategoryController,
+  getCategoryByIdController,
+  updateCategoryController
+} from "../controllers/category.controller";
+import {paginationSchema} from "../schemasJoi/pagination.schema";
+import {schemaVerifierMiddleware} from "../middlewares/schemaVerifier.middleware";
+
+const router = Router();
+
+router.post("/create-category", createCategoryController);
+router.get('/', [schemaVerifierMiddleware({query: paginationSchema})], listCategoryController);
+router.get('/:id', getCategoryByIdController);
+router.post('/update-category/:id', updateCategoryController);
+router.delete('/delete-category/:id', deleteCategoryController);
+
+export { router };
