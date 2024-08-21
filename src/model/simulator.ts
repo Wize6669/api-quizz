8082aa9aeb77cac087e11d5d6ceec886532a2988
XERF99 <francisco.rodriguez@epn.edu.ec>
2024-08-20 20:15:33 -0500

    feat: update models and service simulator.service

--- src/model/simulator.ts
+++ src/model/simulator.ts
@@ -4,7 +4,10 @@ export interface Simulator{
   password: string;
   duration: number;
   navigate: boolean;
-  number_questions?: number;
+  number_of_questions?: number;
+  number_of_sections?: number;
+
 }
 
 export interface SimulatorList extends Omit<Simulator, 'number_questions' | 'navigate'> {}
+export interface SimulatorCreate extends Omit<Simulator, 'password'> {}
