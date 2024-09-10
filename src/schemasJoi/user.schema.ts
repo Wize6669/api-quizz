94e146839285c1503f1996f3134313bed1240654
Wize669 <william.zapata@epn.edu.ec>
2024-09-09 22:32:35 -0500

    feat: Update User schema

--- src/schemasJoi/user.schema.ts
+++ src/schemasJoi/user.schema.ts
@@ -4,7 +4,7 @@ const userSchemaSingUp = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   lastName: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()\\-_+=\\[\\]{}|;:,.<>?]{3,30}$')).required(),
   roleId: Joi.number().required(),
 });
 
@@ -16,8 +16,8 @@ const userSchemaUpdateBody = Joi.object({
 });
 
 const userSchemaChangePasswordBody = Joi.object({
-  temporaryPassword: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  newPassword: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  temporaryPassword: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()\\-_+=\\[\\]{}|;:,.<>?]{3,30}$')).required(),
+  newPassword: Joi.string().pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()\\-_+=\\[\\]{}|;:,.<>?]{3,30}$')).required(),
 });
 
 const userSchemaParams = Joi.object({
